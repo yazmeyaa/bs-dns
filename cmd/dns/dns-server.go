e6ab6e43d3794db0fbed44236c83c255f05ddeb9
Eugene <evgenijantonenkov456@gmail.com>
2024-10-21 11:22:47 +0300

    Refactored dns-server cmd

--- cmd/dns/dns-server.go
+++ cmd/dns/dns-server.go
@@ -10,149 +10,138 @@ import (
 	"time"
 
 	"github.com/pressly/goose/v3"
+	"github.com/redis/go-redis/v9"
 	"github.com/yazmeyaa/bs-dns/internal/config"
 	"github.com/yazmeyaa/bs-dns/internal/dns/answer"
 	"github.com/yazmeyaa/bs-dns/internal/dns/header"
 	"github.com/yazmeyaa/bs-dns/internal/dns/question"
 	"github.com/yazmeyaa/bs-dns/internal/dns/records"
-	"github.com/yazmeyaa/bs-dns/internal/redis"
 	"github.com/yazmeyaa/bs-dns/pkg/db"
+	_redis "github.com/yazmeyaa/bs-dns/pkg/redis"
 )
 
 const Address = "0.0.0.0:53"
 
-var nameToIP = make(map[string][]byte)
-
 func migrateDB(db *sql.DB) error {
 	goose.SetDialect("sqlite3")
-	const migrationsDir string = "pkg/db/migrations"
-
+	migrationsDir := "pkg/db/migrations"
 	if err := goose.Up(db, migrationsDir); err != nil {
 		return err
 	}
-
-	log.Println("Successfull migrated DB")
-
+	log.Println("Successfully migrated DB")
 	return nil
 }
 
-func main() {
+func initServices() (*sql.DB, *redis.Client, error) {
 	db, err := db.InitDB()
 	if err != nil {
-		log.Fatal(err.Error())
-		return
+		return nil, nil, err
 	}
+
 	if err := migrateDB(db); err != nil {
-		log.Fatal(err.Error())
-		return
+		return nil, nil, err
 	}
+
 	config, err := config.New()
-
-	rc, err := redis.InitRedisConnection(context.Background(), config)
-
 	if err != nil {
-		log.Fatal(err.Error())
+		return nil, nil, err
+	}
+
+	rc, err := _redis.InitRedisConnection(context.Background(), config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return db, rc, nil
+}
+
+func handleDNSQuery(ctx context.Context, buf []byte, source *net.UDPAddr, udpConn *net.UDPConn, rc *redis.Client) {
+	if len(buf) < 12 {
+		log.Println("Invalid DNS query, too small")
 		return
 	}
 
-	r := records.DNSRecord{
-		Name:        "1.0.0.127.in-addr.arpa",
-		Label:       "localhost",
-		Description: "Localhost address",
-		IPAddr:      "127.0.0.1",
+	h := header.ReadHeader(buf[:12])
+	h.IsResponse = true
+	q, _ := question.ReadQuestion(buf[12:])
+	h.QDCount = 1
+
+	var res bytes.Buffer
+	if q.QType == 28 {
+		h.ResponseCode = header.RCODE_NOT_IMPLEMENTED
+		res.Write(h.Encode())
+		res.Write(q.Encode())
+		udpConn.WriteToUDP(res.Bytes(), source)
+		return
 	}
 
-	err = r.Save(context.Background(), rc, db)
+	record, err := records.GetDNSRecord(ctx, rc, q.QName)
 	if err != nil {
-		log.Println(err.Error())
-	}
-	udpAddr, err := net.ResolveUDPAddr("udp", Address)
-	if err != nil {
-		log.Fatal("failed to resolve udp address", err)
+		if errors.Is(err, records.ErrRecordNotFound) {
+			log.Printf("Record with domain name [%s] not found", q.QName)
+		} else {
+			log.Printf("Error while getting record: %s", err.Error())
+		}
+		res.Write(h.Encode())
+		res.Write(q.Encode())
+		udpConn.WriteToUDP(res.Bytes(), source)
+		return
 	}
 
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		log.Fatal("failed to to bind to address", err)
+	ans := answer.Answer{
+		Name:   q.QName,
+		QType:  question.TYPE_HOST,
+		QClass: question.CLASS_INTERNET,
+		TTL:    0,
+		Data:   record.GetIPAddrBytes(),
 	}
-	defer udpConn.Close()
+	h.ANCount++
+	h.ResponseCode = header.RCODE_NO_ERROR
 
-	log.Printf("started server on %s", Address)
+	res.Write(h.Encode())
+	res.Write(q.Encode())
+	res.Write(ans.Encode())
+
+	udpConn.WriteToUDP(res.Bytes(), source)
+	log.Printf("Resolved name: %s => %s", q.QName, record.IPAddr)
+}
+
+func startDNSServer(udpConn *net.UDPConn, rc *redis.Client, db *sql.DB) {
 	buf := make([]byte, 512)
 	for {
 		size, source, err := udpConn.ReadFromUDP(buf)
 		start := time.Now()
 		if err != nil {
-			log.Println("failed to receive data", err)
+			log.Println("Failed to receive data", err)
 			continue
 		}
 
-		if size < 12 {
-			log.Println("invalid DNS query, too small")
-			continue
-		}
+		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+		handleDNSQuery(ctx, buf[:size], source, udpConn, rc)
+		cancel()
 
-		h := header.ReadHeader(buf[:12])
-		h.IsResponse = true
-		h.ANCount = 0
-		q, _ := question.ReadQuestion(buf[12:])
-		h.QDCount = 1
-		log.Printf("QUESTION QTYPE: %d", q.QType)
+		log.Printf("Processing time: %d ms", time.Since(start).Milliseconds())
+	}
+}
 
-		var res bytes.Buffer
-
-		if q.QType == 28 {
-			h.ResponseCode = header.RCODE_NOT_IMPLEMENTED
-			res.Write(h.Encode())
-			res.Write(q.Encode())
-
-			_, err = udpConn.WriteToUDP(res.Bytes(), source)
-			if err != nil {
-				log.Println("Failed to send response:", err)
-			}
-			continue
-		}
-
-		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second*10))
-		defer cancel()
-		record, err := records.GetDNSRecord(ctx, rc, q.QName)
-
-		if err != nil {
-			if errors.Is(records.ErrRecordNotFound, err) {
-				log.Printf("Record with domain name [%s] not found", q.QName)
-			} else {
-				log.Printf("Error while getting record: %s", err.Error())
-			}
-			res.Write(h.Encode())
-			res.Write(q.Encode())
-			_, err = udpConn.WriteToUDP(res.Bytes(), source)
-			if err != nil {
-				log.Println("Failed to send response:", err)
-			}
-			continue
-		}
-
-		ans := answer.Answer{
-			Name:   q.QName,
-			QType:  question.TYPE_HOST,
-			QClass: question.CLASS_INTERNET,
-			TTL:    0,
-			Data:   record.GetIPAddrBytes(),
-		}
-		h.ANCount++
-		h.ResponseCode = header.RCODE_NO_ERROR
-
-		res.Write(h.Encode())
-		res.Write(q.Encode())
-		res.Write(ans.Encode())
-
-		_, err = udpConn.WriteToUDP(res.Bytes(), source)
-		if err != nil {
-			log.Println("Failed to send response:", err)
-		}
-
-		log.Printf("Resolved name: %s => %s", q.QName, record.IPAddr)
-		log.Printf("PROCESSING TIME: %d ms", time.Since(start).Milliseconds())
+func main() {
+	db, rc, err := initServices()
+	if err != nil {
+		log.Fatal(err.Error())
+		return
 	}
 
+	udpAddr, err := net.ResolveUDPAddr("udp", Address)
+	if err != nil {
+		log.Fatal("Failed to resolve UDP address", err)
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal("Failed to bind to address", err)
+	}
+	defer udpConn.Close()
+
+	log.Printf("Started server on %s", Address)
+	startDNSServer(udpConn, rc, db)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/pressly/goose/v3 v3.22.1
 	github.com/redis/go-redis/v9 v9.7.0
 )
 
@@ -11,7 +12,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
-	github.com/pressly/goose/v3 v3.22.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
